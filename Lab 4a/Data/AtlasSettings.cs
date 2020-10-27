9c1a79d6268a8b70abadd3632e52fcdc690c9668
Hayden Smith <haydensmith20.hs@gmail.com>
2020-10-26 20:46:06 -0600

    Add project files.

---
+++ Lab 4a/Data/AtlasSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_4a.Data
+{
+	public class AtlasSettings : IAtlasSettings
+	{
+		public string Collection { get; set; }
+		public string Database { get; set; }
+		public string ConnectionString { get; set; }
+	}
+	public interface IAtlasSettings
+	{
+		string Collection { get; set; }
+		string Database { get; set; }
+		string ConnectionString { get; set; }
+	}
+}
---
+++ Lab 4a/Models/_Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Lab_4a.Models
+{
+    public class _Task
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string Text { get; set; }
+        public bool Done { get; set; }
+        public int Date { get; set; }
+    }
+}
